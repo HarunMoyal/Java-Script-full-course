31b9a7c52a211959f56556a2371aea04cff43e90
HarunMoyal <harunmoyal.hm@gmail.com>
2023-06-20 02:04:27 +0530

    math object

---
+++ math_object.js
@@ -0,0 +1,20 @@
+//mathes object mai js ke defin maths function hote hai
+// jinhe hum direct call kr sakte hai
+
+
+console.log(Math.PI);// output:- 3.141592653589793 auto value print kr dega
+let a =  Math.round(4.7);// yeh value ko round off set kr ke batayega
+let b = Math.round(4.2);
+console.log(a);
+console.log(b);
+let c = Math.floor(4.1);// yeh pali value ko hi batayega
+
+console.log(c);
+
+let d = Math.pow(10,2);// power function
+console.log(d);
+
+let e = Math.min(4,50,12,3);// minimum value print kr dega
+console.log(e);
+let f = Math.max(30,40,90,101);//maximum value print kr dega
+console.log(f);
