5b633aaf9717bf0550e0a76d44f24b042780b66f
HarunMoyal <harunmoyal.hm@gmail.com>
2023-06-19 15:50:29 +0530

    Anonymous_Function

---
+++ Anonymous_Function.js
@@ -0,0 +1,17 @@
+//jis function ka kuch naam na ho usse Anonymous function kahte hai
+//function(){
+  //  console.log("hello world");
+//}
+// Anonymous function ko variable mai diclair kr sakte hai
+/*let fun = function(){ //isko function expression kahte hai
+    console.log ("heloo world");
+};
+
+show();*/
+
+
+//setTimeout(function name,time in mili second );
+// hm setTimeout mai chahe toh Anonymous function bna sakte hai
+setTimeout(function  (){
+    document.write("Anonymous Function hello ");
+} , 5000);
